3064ddd2f2aa0d336f5bdc00b472a3b7ac7c5fbd
Roman Storm <rstormsf@gmail.com>
2017-12-29 23:07:11 -0800

    load addresses based on netId

--- src/contracts/KeysManager.contract.js
+++ src/contracts/KeysManager.contract.js
@@ -1,13 +1,14 @@
 import KeysManagerAbi from './keysManager.abi.json'
 import Web3 from 'web3';
-import {KEYS_MANAGER_ADDRESS} from './addresses';
+import networkAddresses from './addresses';
 
-console.log('Keys Manager', KEYS_MANAGER_ADDRESS);
 
 export default class KeysManager {
-  constructor({web3}){
+  constructor({web3, netId}){
     let web3_10 = new Web3(web3.currentProvider);
+    const {KEYS_MANAGER_ADDRESS} = networkAddresses(netId);
     this.keysInstance = new web3_10.eth.Contract(KeysManagerAbi, KEYS_MANAGER_ADDRESS);
+    console.log('Keys Manager', KEYS_MANAGER_ADDRESS);
   }
   async isVotingActive(votingKey) {
     return await this.keysInstance.methods.isVotingActive(votingKey).call();
--- src/contracts/PoaConsensus.contract.js
+++ src/contracts/PoaConsensus.contract.js
@@ -1,12 +1,13 @@
 import poaConsensusAbi from './poaConsensus.abi.json'
 import Web3 from 'web3';
-import {POA_ADDRESS} from './addresses';
+import networkAddresses from './addresses';
 
-console.log('POA Address ' , POA_ADDRESS)
 export default class POAConsensus {
-  constructor({web3}){
+  constructor({web3, netId}){
     let web3_10 = new Web3(web3.currentProvider);
+    const {POA_ADDRESS} = networkAddresses(netId);
     this.poaInstance = new web3_10.eth.Contract(poaConsensusAbi, POA_ADDRESS);
+    console.log('POA Address ' , POA_ADDRESS)
   }
   async getValidators(){
     return await this.poaInstance.methods.getValidators().call();
