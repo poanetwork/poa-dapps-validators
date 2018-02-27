c8f7c5802ee279b3a0cd5b4e76c2069da963f9f5
viktor <baranov.viktor.27@gmail.com>
2018-02-27 14:21:40 +0300

    global helpers

---
+++ src/helpers.js
@@ -0,0 +1,17 @@
+import swal from 'sweetalert';
+
+function generateAlert(icon, title, msg) {
+	var content = document.createElement("div");
+    content.innerHTML = `<div style="line-height: 1.6;">${msg}</div>`
+	swal({
+	    icon: icon,
+	    title: title,
+	    content: content
+  	});
+}
+
+let helpers = {
+	generateAlert
+}
+
+export default helpers
