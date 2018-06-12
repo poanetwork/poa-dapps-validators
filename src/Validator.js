8d3b2821f4732446c95499f253e5ba124047bb3d
Vadim Arasev <varasev@gmail.com>
2018-06-12 16:49:03 +0300

    (Fix) Remove displaying of validator's index from "Pending changes" tab

--- src/Validator.js
+++ src/Validator.js
@@ -10,7 +10,8 @@ class Validator extends Component {
       this.setState({confirmation: confirmation[0]})
     })
   }
-  render(){
+
+  render() {
     let {
       address,
       firstName,
@@ -25,13 +26,19 @@ class Validator extends Component {
       index,
       children,
     } = this.props;
-    let confirmations = this.props.methodToCall === 'getAllValidatorsData' ? '' : <div className="confirmations">
-          <div>{this.state.confirmation} confirmations</div>
-      </div>
+
+    const showAllValidators = this.props.methodToCall === 'getAllValidatorsData';
+
+    const confirmations = showAllValidators ? '' : <div className="confirmations">
+      <div>{this.state.confirmation} confirmations</div>
+    </div>;
+
+    const indexAndAddress = showAllValidators ? `#${index}. ${address}` : address;
+
     return(
       <div className="validators-i">
       <div className="validators-header">
-        <div className="validators-header--address">#{index}. {address}</div>
+        <div className="validators-header--address">{indexAndAddress}</div>
         <div>{confirmations}</div>
         <div>{children}</div>
       </div>
