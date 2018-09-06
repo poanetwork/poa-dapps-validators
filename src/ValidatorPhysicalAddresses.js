59e9fca28a059aea247dd30e4e61fe3e1c81fce5
Jorge Luis Shirai <jlshirai@gmail.com>
2018-09-06 12:49:13 -0300

    Add title attr to ValidatorPhysicalAddresses icon.

--- src/ValidatorPhysicalAddresses.js
+++ src/ValidatorPhysicalAddresses.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 const ICON_CONFIRMED_CLASSNAME = 'data-icon--confirmed'
 const ICON_UNCONFIRMED_CLASSNAME = 'data-icon--unconfirmed'
+const ICON_CONFIRMED_TOOLTIP = 'Confirmed Address'
+const ICON_UNCONFIRMED_TOOLTIP = 'Unconfirmed Address'
 
 const ValidatorPhysicalAddress = ({ physicalAddresses }) => {
   const confirmedAddresses = physicalAddresses.filter(a => a.isConfirmed)
@@ -24,13 +26,19 @@ const PhysicalAddressValue = ({ addresses }) => {
   let textAlign = addresses.length > 1 ? 'text-align-left' : 'text-align-right'
   let physicalAddressValueClassName = `validator-physical-address__value text-capitalized ${textAlign}`
 
-  return addresses.map((address, index) => (
-    <p className={physicalAddressValueClassName} key={index}>
-      {`${address.fullAddress} `}
-      <span className="text-whitespace-nowrap">{`${address.us_state.toUpperCase()}, ${address.postal_code}`}</span>
-      <i className={address.isConfirmed ? ICON_CONFIRMED_CLASSNAME : ICON_UNCONFIRMED_CLASSNAME} />
-    </p>
-  ))
+  return addresses.map((address, index) => {
+    const iconProps = {
+      className: address.isConfirmed ? ICON_CONFIRMED_CLASSNAME : ICON_UNCONFIRMED_CLASSNAME,
+      title: address.isConfirmed ? ICON_CONFIRMED_TOOLTIP : ICON_UNCONFIRMED_TOOLTIP
+    }
+    return (
+      <p className={physicalAddressValueClassName} key={index}>
+        {`${address.fullAddress} `}
+        <span className="text-whitespace-nowrap">{`${address.us_state.toUpperCase()}, ${address.postal_code}`}</span>
+        <i {...iconProps} />
+      </p>
+    )
+  })
 }
 
 export default ValidatorPhysicalAddress
