8f6307bc1ef8f63d8520d4ed088322703d40ffa5
Max Alekseenko <nnwen0609@gmail.com>
2019-09-12 22:06:08 +0300

    change errors text

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -4,10 +4,8 @@ messages.wrongRepo = function(repo) {
 }
 messages.invalidaVotingKey =
   'The key is not a valid voting Key! Please make sure you have loaded the correct voting key in Metamask / Nifty Wallet.'
-messages.noMetamaskAccount = `Your MetaMask is locked.
-Please choose your voting key in MetaMask and reload the page.
-Check POA Network <a href='https://github.com/poanetwork/wiki' target='blank'>wiki</a> for more info.`
-messages.networkMatchError = 'Networks do not match. Change the network in Metamask'
+messages.noMetamaskAccount = 'Your MetaMask is locked.'
+messages.networkMatchError = 'Networks do not match. Change the network in Metamask.'
 
 module.exports = {
   messages
