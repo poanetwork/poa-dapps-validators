082f48b280a4e47c8cd527e2e1b82340271ed846
Vadim <varasev@gmail.com>
2019-11-12 12:32:17 +0300

    Improve message texts

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -5,13 +5,13 @@ messages.wrongRepo = function(repo) {
   return `There is no such file in configured repo ${repo}`
 }
 messages.invalidaVotingKey =
-  'The current key is not a valid Voting Key! Please make sure you have loaded the correct Voting Key in Metamask / Nifty Wallet.'
-messages.noMetamaskAccount = 'Your MetaMask is locked.'
-messages.noMetamaskFound = 'Metamask is not found.'
+  'The current key is not a valid Voting Key! Please make sure you have loaded the correct Voting Key in MetaMask / Nifty Wallet.'
+messages.noMetamaskAccount = 'Your MetaMask / Nifty Wallet is locked.'
+messages.noMetamaskFound = 'MetaMask / Nifty Wallet is not found.'
 
 messages.networkMatchError = function(netId) {
   const networkName = getNetworkFullName(Number(netId))
-  return `Networks in DApp and MetaMask do not match. Switch MetaMask to <b>${networkName}</b> or change the network in DApp.`
+  return `Networks in DApp and MetaMask (Nifty Wallet) do not match. Switch MetaMask / Nifty Wallet to <b>${networkName}</b> or change the network in DApp.`
 }
 
 export default messages
